250ad593b373fe00590a8c5083e9a483bceb62a0
rawankadi <109313595+rawankadi@users.noreply.github.com>
2022-11-23 22:10:16 +0200

    my first commit

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import routes from './routes/index';
 import { connectDb } from "./db/index";
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import path from "path";
 
 const app = express();
 app.use(bodyParser.urlencoded());
@@ -10,6 +11,13 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(routes);
 
+app.use(express.static(path.join(__dirname,"https://taskmoveo.herokuapp.com/")));
+
+app.get('*', (req ,res) => {
+    res.sendFile(path.join(__dirname,'https://taskmoveo.herokuapp.com/HomePage','index.html'));
+});
+
 connectDb().then(async () => {
-    app.listen(3001, () => console.log("Listening on http://localhost:3001"));
-    });
+    app.listen(process.env.PORT || 3001, () => console.log("Listening on http://localhost:3001"));
+});
+
