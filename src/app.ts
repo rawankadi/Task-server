7c024e9476cb72d1620ebb52cda7734eef3b3904
rawankadi <109313595+rawankadi@users.noreply.github.com>
2022-11-24 21:09:03 +0200

    Allow front to call

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,7 @@ app.use(routes);
 // });
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Origin", "https://taskmoveo.herokuapp.com/")
     res.header(
       "Access-Control-Allow-Headers",
       "Origin, X-Requested, Content-Type"
